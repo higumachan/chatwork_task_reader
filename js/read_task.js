ef261da38580e80aed2fa97c54756b8ad9670bcf
Yuta Hinokuma <harekumo792154@gmail.com>
2014-07-31 14:45:48 +0900

    初回読み込みのタスクを特別視

--- js/read_task.js
+++ js/read_task.js
@@ -1,8 +1,30 @@
 
 $(function (){
-	var tasks = $("#_subRoomTaskList ._taskNameContent");
+	var task_tags = $("#_subRoomTaskList>li");
+	localStorage = chrome.storage.local;
 
-	for (var i = 0; i < tasks.length; i++){
-		console.log($(tasks[i]).text());
+	var text, id;
+	var tasks = [];
+	var nyr_tasks = []; //Not Yet Read tasks.
+	for (var i = 0; i < task_tags.length; i++){
+		id = $(task_tags[i]).attr("data-task-id");
+		text = $(task_tags[i]).text();
+		var task = {
+			id: id,
+			text: text,
+		};
+		tasks.push(task);
+
+		if (!localStorage.getItem(id)){
+			nyr_tasks.push(task);
+			/*
+			var add = {};
+			add[id] = true;
+			*/
+			localStorage.setItem(id, true);
+		}
 	}
+
+	console.log(tasks);
+	console.log(nyr_tasks);
 });
